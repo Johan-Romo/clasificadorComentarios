18a5698e6270c5918b0767a30ebfdaae56667378
johan <jaromo3@espe.edu.ec>
2025-02-13 23:53:56 -0500

    pool

--- back/server.js
+++ back/server.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const commentsRoutes = require('./routes/commentsRoutes');
-
+const pool = require('./config/db');
 const app = express();
 
 // Middlewares
